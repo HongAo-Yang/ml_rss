ad40f3e7022630a743472bf6969abfe6949fe414
Hong-Ao Yang <yha17@mails.tsinghua.edu.cn>
2020-10-17 00:12:54 +0800

    modified clean.sh

--- clean.sh
+++ clean.sh
@@ -4,3 +4,4 @@ rm -f *.log
 rm -f *.err
 rm -f *.extxyz
 rm -f tmp.*
+rm -f RSS_tmp/*
--- main2.py
+++ main2.py
@@ -2,7 +2,7 @@ from RSS import *
 import os
 
 if __name__ == "__main__":
-    num_process = 6
+    num_process = 4
     my_timer = timer()
     my_timer.start()
 
@@ -27,49 +27,49 @@ if __name__ == "__main__":
                         num_process=num_process)
     my_timer.barrier()
 
-    os.system("cat RSS_results.out.*_*.extxyz > RSS_results.extxyz")
+    # os.system("cat RSS_results.out.*_*.extxyz > RSS_results.extxyz")
 
-    # select from RSS results with Boltzmann weighted flat histogram
-    select_by_flat_histo(input_file_name='RSS_results.extxyz',
-                         minim_select_flat_histo_n=20,
-                         kT=0.3,
-                         output_file_name='selected_histo.RSS_results.extxyz')
+    # # select from RSS results with Boltzmann weighted flat histogram
+    # select_by_flat_histo(input_file_name='RSS_results.extxyz',
+    #                      minim_select_flat_histo_n=20,
+    #                      kT=0.3,
+    #                      output_file_name='selected_histo.RSS_results.extxyz')
 
-    # select from RSS minima using CUR on descriptor vector
-    calculate_descriptor_vec(input_file_name='selected_histo.RSS_results.extxyz',
-                             selection_descriptor='soap l_max=12 n_max=12 atom_sigma=0.75 cutoff=10',
-                             output_file_name='descriptor_vec.selected_histo.RSS_results.extxyz',
-                             num_process=num_process)
-    my_timer.barrier()
+    # # select from RSS minima using CUR on descriptor vector
+    # calculate_descriptor_vec(input_file_name='selected_histo.RSS_results.extxyz',
+    #                          selection_descriptor='soap l_max=12 n_max=12 atom_sigma=0.75 cutoff=10',
+    #                          output_file_name='descriptor_vec.selected_histo.RSS_results.extxyz',
+    #                          num_process=num_process)
+    # my_timer.barrier()
 
-    select_by_descriptor(input_file_name='descriptor_vec.selected_histo.RSS_results.extxyz',
-                         random_struct_num=10,
-                         selection_method="CUR",
-                         method_kwargs={'kernel_exp': 4},
-                         output_file_name="selected_CUR.RSS_results.extxyz")
-    my_timer.barrier()
+    # select_by_descriptor(input_file_name='descriptor_vec.selected_histo.RSS_results.extxyz',
+    #                      random_struct_num=10,
+    #                      selection_method="CUR",
+    #                      method_kwargs={'kernel_exp': 4},
+    #                      output_file_name="selected_CUR.RSS_results.extxyz")
+    # my_timer.barrier()
 
-    ## select from trajectories corresponding to selected minima ##########################################################
-    # 1. select trajectories
-    select_traj_of_minima(infiles=['selected_CUR.RSS_results.extxyz'],
-                          outfile='selected_RSS_trajectories.extxyz')   # 'infiles' should be list
+    # ## select from trajectories corresponding to selected minima ##########################################################
+    # # 1. select trajectories
+    # select_traj_of_minima(infiles=['selected_CUR.RSS_results.extxyz'],
+    #                       outfile='selected_RSS_trajectories.extxyz')   # 'infiles' should be list
 
-    # 2. select from RSS trajectories by Boltzmann-weighted flat histogram
-    select_by_flat_histo(input_file_name='selected_RSS_trajectories.extxyz',
-                         minim_select_flat_histo_n=20,
-                         kT=0.3,
-                         output_file_name='selected_histo.selected_RSS_trajectories.extxyz')
+    # # 2. select from RSS trajectories by Boltzmann-weighted flat histogram
+    # select_by_flat_histo(input_file_name='selected_RSS_trajectories.extxyz',
+    #                      minim_select_flat_histo_n=20,
+    #                      kT=0.3,
+    #                      output_file_name='selected_histo.selected_RSS_trajectories.extxyz')
 
-    # 3. calculate descriptor vectors for all low symmetry random cells
-    calculate_descriptor_vec(input_file_name='selected_histo.selected_RSS_trajectories.extxyz',
-                             selection_descriptor='soap l_max=12 n_max=12 atom_sigma=0.75 cutoff=10',
-                             output_file_name='descriptor_vec.selected_histo.selected_RSS_trajectories.extxyz',
-                             num_process=num_process)
-    my_timer.barrier()
+    # # 3. calculate descriptor vectors for all low symmetry random cells
+    # calculate_descriptor_vec(input_file_name='selected_histo.selected_RSS_trajectories.extxyz',
+    #                          selection_descriptor='soap l_max=12 n_max=12 atom_sigma=0.75 cutoff=10',
+    #                          output_file_name='descriptor_vec.selected_histo.selected_RSS_trajectories.extxyz',
+    #                          num_process=num_process)
+    # my_timer.barrier()
 
-    select_by_descriptor(input_file_name='descriptor_vec.selected_histo.selected_RSS_trajectories.extxyz',
-                         random_struct_num=10,
-                         selection_method="CUR",
-                         method_kwargs={'kernel_exp': 4},
-                         output_file_name="selected_by_desc.selected_RSS_trajectories.extxyz")
-    my_timer.barrier()
+    # select_by_descriptor(input_file_name='descriptor_vec.selected_histo.selected_RSS_trajectories.extxyz',
+    #                      random_struct_num=10,
+    #                      selection_method="CUR",
+    #                      method_kwargs={'kernel_exp': 4},
+    #                      output_file_name="selected_by_desc.selected_RSS_trajectories.extxyz")
+    # my_timer.barrier()
