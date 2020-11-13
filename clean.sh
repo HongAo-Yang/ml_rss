8ba452496593c0d818669a2a62b4d4c2e507b0d0
HongAo-Yang <yha17@mails.tsinghua.edu.cn>
2020-11-13 11:18:14 +0800

    arranged files

--- clean.sh
+++
@@ -1,7 +0,0 @@
-rm -rf VASP_inputs_*
-rm -rf LAMMPS_inputs_*
-rm -f *.log
-rm -f *.err
-rm -f *.extxyz
-rm -f tmp.*
-rm -f RSS_tmp/*
--- run.sh
+++
@@ -1,7 +0,0 @@
-start_tm=$(date +%s%N)
-
-mpirun -np 8 python main2.py
-
-end_tm=$(date +%s%N)
-use_tm=$(echo $end_tm $start_tm | awk '{ print ($1 - $2) / 1000000000}')
-echo total_time $use_tm
